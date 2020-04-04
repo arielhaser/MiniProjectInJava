0c2dc0b8771814c2ad1d54b39231de20bcb0f515
Ariel Haser <arielosh8@gmail.com>
2020-04-05 00:16:08 +0300

    first commit - targil 3 (tests has established firstly)

--- src/unittests/geometries/GeometriesTest.java
+++ src/unittests/geometries/GeometriesTest.java
@@ -1,6 +1,12 @@
 package geometries;
 
 import org.junit.Test;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -12,7 +18,41 @@ public class GeometriesTest {
 
     @Test
     public void findIntersections() {
-        //TC01:
+        Triangle tr = new Triangle(new Point3D(2,3,2),new Point3D(3,1,2),new Point3D(4,-4,4));
+        Sphere sp = new Sphere(2, new Point3D(0,-2,2));
+        Plane pl = new Plane(new Point3D(1,2,4),new Point3D(4,5,7),new Point3D(8,8,9));
+        ArrayList<Intersectable> temp_list = new ArrayList<>();
+        temp_list.add(tr);
+        temp_list.add(sp);
+        temp_list.add(pl);
+        Geometries ge = new Geometries(temp_list);
+        Geometries ge_empty = new Geometries();
 
+        // ============ Equivalence Partitions Tests ==============
+        //TC01: some of shapes inserted (but not all of them) (sphere and plane == 3 points)
+        List<Point3D> result = ge.findIntersections(new Ray(new Point3D(-3,0,0),
+                new Vector(4.0091500472, -2.8390318306, 2.5269172321)));
+        assertEquals("Wrong number of points", 3, result.size());
+
+        // =============== Boundary Values Tests ==================
+        //TC02: empty body list (0 points)
+        result = ge_empty.findIntersections(new Ray(new Point3D(-3,0,0),
+                new Vector(4.0091500472, -2.8390318306, 2.5269172321)));
+        assertEquals("Ray's line out of shape", null, result);
+
+        //EC03: not single shape has inserted (0 points)
+        result = ge.findIntersections(new Ray(new Point3D(0,3,0),
+                new Vector(-1.5511254045, -3.4504801707, 1.6501650632)));
+        assertEquals("Ray's line out of shape", null, result);
+
+        //EC04: one shape inserted (plane == 1 points)
+        result = ge.findIntersections(new Ray(new Point3D(0,3,0),
+                new Vector(3,3,3)));
+        assertEquals("Wrong number of points", 1, result.size());
+
+        //EC05: all shapes inserted (4 points)
+        result = ge.findIntersections(new Ray(new Point3D(-3,0,0),
+                new Vector(6.4595459051, -1.783079407, 3.2426583979)));
+        assertEquals("Wrong number of points", 4, result.size());
     }
 }
